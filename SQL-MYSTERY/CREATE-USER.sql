e13816529fc06fe68d5e265beced808237cab014
duducaa <duduca1881@gmail.com>
2023-06-17 19:13:44 -0300

    Revert "added the tables for tablespace pontuacao and create user for web requests"
    This reverts commit 3e4fe64ab539bbdb397edb46afb4064406ccd5b9.

--- SQL-MYSTERY/CREATE-USER.sql
+++
@@ -1,5 +0,0 @@
-#CRIAÇÃO DO USUÁRIO
-DROP USER 'web-user'@'localhost';
-CREATE USER 'web-user'@'localhost' IDENTIFIED BY 'sql_mystery';
-GRANT SELECT ON sql_mystery.* to 'web-user'@'localhost';
-GRANT INSERT ON sql_mystery.tb_solucao to 'web-user'@'localhost';
