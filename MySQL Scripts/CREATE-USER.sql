3c34d99c42b70c27f7299a59bba63bd7ca7249f3
duducaa <duduca1881@gmail.com>
2023-06-17 21:59:36 -0300

    updated UML and create the script for create the user for web

---
+++ MySQL Scripts/CREATE-USER.sql
@@ -0,0 +1,4 @@
+drop user 'web-user'@'localhost';
+CREATE USER 'web-user'@'localhost' IDENTIFIED BY 'web1234web';
+GRANT SELECT ON SQL_MYSTERY.* TO 'web-user'@'localhost';
+GRANT INSERT ON SQL_MYSTERY.TB_SOLUCAO TO 'web-user'@'localhost';
