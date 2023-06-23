902d5728e88f4f66b21cbc11ad85a820458d78e7
duducaa <duduca1881@gmail.com>
2023-06-23 17:53:12 -0300

    start to create user route

---
+++ SQL-MYSTERY/MyProject.BLL.Service/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.BLL.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost(Name = "SignUp")]
+        public ActionResult SignUp([FromBody] string login, [FromBody] string email, [FromBody] string password)
+        {
+
+        }
+    }
+}
